192b2dd1a096edb83fd770d24a9ebc104cae9c2e
obliviionx <87861344+obliviionx@users.noreply.github.com>
2022-05-28 23:11:27 -0500

    Add files via upload

---
+++ Clase3Corte/Clase3Corte/Data/Libro.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Clase3Corte.Data
+{
+    public class Libro
+    {
+        [Key]
+        public int IdLibro { get; set; }
+        public string? Titulo { get; set; }
+        public string? Editorial { get; set; }
+        public string? Autor { get; set; }
+        public int Pagina { get; set; }
+        public string? Descripcion { get; set; }
+    }
+}
---
+++ Clase3Corte/Clase3Corte/Data/Movies.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Clase3Corte.Data
+{
+    public class Movies
+    {
+        [Key]
+        public int IdMovie { get; set; }
+        public string Year { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public string Genre { get; set; }
+        public double Rating { get; set; }
+    }
+}
